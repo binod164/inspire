7dc8fa618fd4cd9c8464320b2f1fbc2629819624
binod164 <binod7_grg@hotmail.com>
2022-04-20 15:26:55 -0500

    Create new model AppliedJob

--- myapp/models.py
+++ myapp/models.py
@@ -18,6 +18,8 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
+    jobs = db.relationship('AppliedJob', backref='author', lazy=True)
+
 
     def __init__(self, email, username, password):
         self.email = email
@@ -31,3 +33,27 @@ class User(db.Model, UserMixin):
     def __repr__(self):
         return f"Username {self.username}"
 
+class AppliedJob(db.Model):
+    __tablename__ = 'applied_jobs'
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    title = db.Column(db.String(140), nullable=False)
+    company = db.Column(db.String(140), nullable=False)
+    date_applied = db.Column(db.String(140), nullable=False)
+    accepted = db.Column(db.Boolean, default=False, nullable=False)
+    in_process = db.Column(db.Boolean, default=False, nullable=False)
+    rejected = db.Column(db.Boolean, default=False, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+
+    def __init__(self, title, company, date_applied, accepted, in_process,rejected, user_id):
+        self.title = title
+        self.company = company
+        self.date_applied = date_applied
+        self.accepted = accepted
+        self.in_process = in_process
+        self.rejected = rejected
+        self.user_id = user_id
+    
+    def __repr__(self):
+        return f"Job ID: {self.id} -- Date: {self.date} --- Title: {self.Title}"
