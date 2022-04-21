16be0659d1f5145fd9b507a9b5f4fc82e72fd653
binod164 <binod7_grg@hotmail.com>
2022-04-21 10:05:18 -0500

    Create a route to show applied jobs

--- myapp/applied_jobs/views.py
+++ myapp/applied_jobs/views.py
@@ -8,7 +8,7 @@ applied_jobs = Blueprint('applied_jobs', __name__)
 
 @applied_jobs.route('/create', methods=['GET', 'POST'])
 @login_required
-def create_post():
+def create_appliedjob():
     form = AppliedJobForm()
     if form.validate_on_submit():
         applied_job = AppliedJob(title=form.title.data, company=form.company.data, 
@@ -20,3 +20,10 @@ def create_post():
         print('Job post was created')
         return redirect(url_for('core.index'))
     return render_template('create_post.html', form=form)
+
+# Make sure the blog_post_id is an integer!
+
+@applied_jobs.route('/<int:applied_job_id>')
+def applied_job(applied_job_id):
+    applied_job = AppliedJob.query.get_or_404(applied_job_id) 
+    return render_template('applied_job.html', title=applied_job.title, date=applied_job.date, job=applied_job_id)
