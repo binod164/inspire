fbdad27f115fe7c474f3c8fd69d3de6cae8875c2
binod164 <binod7_grg@hotmail.com>
2022-04-21 14:06:02 -0500

    add nav item to add applied job

--- myapp/core/views.py
+++ myapp/core/views.py
@@ -1,13 +1,17 @@
 # core/views.py 
 
 from flask import render_template, request, Blueprint
+from myapp import applied_jobs
+from myapp.applied_jobs.views import applied_job
+from myapp.models import AppliedJob
 
 core = Blueprint('core', __name__)
 
 @core.route('/')
 def index():
-    
-    return render_template('index.html')
+    page = request.args.get('page', 1, type=int)
+    applied_jobs = AppliedJob.query.order_by(AppliedJob.date.desc()).paginate(page=page, per_page=5)
+    return render_template('index.html', applied_jobs=applied_jobs)
 
 @core.route('/info')
 def info():
